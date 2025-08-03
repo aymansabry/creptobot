ac5e2f45af9b79ce2f09cda49067ea1594cd7c5b
aymansabry <zeioszeios0@gmail.com>
2025-08-04 00:22:50 +0300

    Update db.py

--- bot/db.py
+++ bot/db.py
@@ -1,12 +1,11 @@
-import sqlalchemy
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import SQLModel, create_engine
+import os
 
-from bot.models import User
+# استخدم SQLite - سيتم إنشاء ملف قاعدة البيانات في نفس مجلد المشروع
+DATABASE_URL = "sqlite:///database.db"
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(DATABASE_URL, echo=False)
 
 async def create_db_and_tables():
+    import bot.models  # تأكد من استيراد جميع النماذج
     SQLModel.metadata.create_all(engine)
-
-def get_session():
-    return Session(engine)
