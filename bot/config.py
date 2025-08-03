fc71ce2ff001059850adace8b01cbdc08eacc55b
aymansabry <zeioszeios0@gmail.com>
2025-08-03 22:32:43 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_WALLET = os.getenv("OWNER_WALLET")  # محفظة مالك البوت
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ETH_NODE = os.getenv("ETH_NODE")  # عنوان العقدة الخاصة بـ Web3
+GPT_API_KEY = os.getenv("GPT_API_KEY")
+ADMIN_WALLET = os.getenv("ADMIN_WALLET")  # محفظة العمولة
+
