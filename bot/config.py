3cc1eeb90546e1734558bdd5c7f6c0da4e79f1c8
aymansabry <zeioszeios0@gmail.com>
2025-08-03 23:53:12 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-GPT_API_KEY = os.getenv("GPT_API_KEY")
-ADMIN_WALLET = os.getenv("ADMIN_WALLET")  # محفظة العمولة المركزية
+DATABASE_URL = os.getenv("DATABASE_URL")
