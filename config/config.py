7891a4e607b33517227a6bb6e9c9ccb8a6c7b3ed
aymansabry <zeioszeios0@gmail.com>
2025-08-05 12:25:05 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,8 +1,12 @@
+# config/config.py
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(i) for i in os.getenv("ADMIN_IDS", "").split(",") if i]
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
