d718dceb7c224a1a392ed0db1bc85a66ae567efb
aymansabry <zeioszeios0@gmail.com>
2025-08-05 11:44:29 +0300

    Delete bot directory

--- bot/main.py
+++
@@ -1,7 +0,0 @@
-from telegram.ext import Application
-from config import TELEGRAM_TOKEN
-from handlers import setup_handlers
-
-app = Application.builder().token(TELEGRAM_TOKEN).build()
-setup_handlers(app)
-app.run_polling()
