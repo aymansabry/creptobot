c53c5f5caa3a74d212a650777bd9d3aa5d14c293
aymansabry <zeioszeios0@gmail.com>
2025-08-05 11:47:21 +0300

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,1 +1,18 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from app.database.session import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, unique=True, index=True)
+    wallet_address = Column(String)
+    is_admin = Column(Boolean, default=False)
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    tx_type = Column(String)
+    amount = Column(Float)
+    tx_hash = Column(String)
 
