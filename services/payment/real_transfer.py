485264053cce3bafc72267a2449fc1ea8f64e553
aymansabry <zeioszeios0@gmail.com>
2025-08-06 13:10:29 +0300

    Update real_transfer.py

--- services/payment/real_transfer.py
+++ services/payment/real_transfer.py
@@ -1,26 +1,21 @@
 from tronpy import Tron
+from tronpy.providers import HTTPProvider
 from config import config
 
-class PaymentGateway:
-    def __init__(self, config):
-        self.client = Tron(network='mainnet')
+class TronPayment:
+    def __init__(self):
+        self.client = Tron(
+            provider=HTTPProvider(endpoint_uri="https://api.trongrid.io"),
+            network='mainnet'
+        )
         self.private_key = config.TRON_PRIVATE_KEY
-    
-    async def distribute_profits(self, user_id: int, amount: float):
-        bot_share = amount * config.BOT_COMMISSION
-        user_share = amount - bot_share
         
-        # تحويل عمولة البوت
-        await self._send_usdt(config.ADMIN_WALLET, bot_share)
-        
-        # تحويل ربح المستخدم (يجب استبدال بمحفظة المستخدم الفعلية)
-        await self._send_usdt("USER_TRON_WALLET", user_share)
-    
-    async def _send_usdt(self, to_address: str, amount: float):
-        contract = self.client.get_contract(config.TRON_USDT_CONTRACT)
+    async def send_usdt(self, to_address: str, amount: float):
+        contract = self.client.get_contract("TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t")
         txn = (
-            contract.functions.transfer(to_address, int(amount * 1_000_000))
+            contract.functions.transfer(to_address, int(amount * 10**6))
             .with_owner(config.ADMIN_WALLET)
+            .fee_limit(10_000_000)
             .build()
             .sign(self.private_key)
         )
