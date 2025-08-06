f4e7d33caf9b5cd7822c673cc7716be8fb862ed7
aymansabry <zeioszeios0@gmail.com>
2025-08-06 11:43:53 +0300

    Update and rename database.py to db.py

--- database/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from config.config import settings
-
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
