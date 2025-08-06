384b4d2c2028388b2bcd9ce490cbd964a62c8ddc
aymansabry <zeioszeios0@gmail.com>
2025-08-06 12:39:19 +0300

    Create encryption.py

---
+++ database/encryption.py
@@ -0,0 +1,12 @@
+from cryptography.fernet import Fernet
+import base64
+
+class CryptoManager:
+    def __init__(self, key: str):
+        self.cipher = Fernet(base64.urlsafe_b64encode(key.encode().ljust(32)[:32]))
+    
+    def encrypt(self, data: str) -> str:
+        return self.cipher.encrypt(data.encode()).decode()
+    
+    def decrypt(self, token: str) -> str:
+        return self.cipher.decrypt(token.encode()).decode()
