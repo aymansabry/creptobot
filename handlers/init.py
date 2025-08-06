f808a6a4c04461f0b5acf613a82dab49029769b2
aymansabry <zeioszeios0@gmail.com>
2025-08-06 14:49:23 +0300

    Update init.py

--- handlers/init.py
+++ handlers/init.py
@@ -1,11 +1,5 @@
-from .commands import router as commands_router
 from .admin import router as admin_router
+from .commands import router as commands_router
 from .deals import router as deals_router
 
-routers = [
-    commands_router,
-    admin_router,
-    deals_router
-]
-
-__all__ = ['routers']
+__all__ = ['admin_router', 'commands_router', 'deals_router']
