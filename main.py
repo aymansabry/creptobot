f09b8f4d70d26388d83825a67dbc440806ef8d17
aymansabry <zeioszeios0@gmail.com>
2025-08-06 11:48:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import BotCommand
+from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
-from handlers.user_handlers import register_user_handlers
-from handlers.admin_handlers import register_admin_handlers
-from database.core import create_tables
-
-bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher()
-
-async def set_commands(bot: Bot):
-    await bot.set_my_commands([
-        BotCommand(command="/start", description="ابدأ"),
-        BotCommand(command="/admin", description="لوحة المدير")
-    ])
+from src.handlers import setup_handlers
 
 async def main():
-    await create_tables()
-    register_user_handlers(dp)
-    register_admin_handlers(dp)
-    await set_commands(bot)
+    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+    dp = Dispatcher(storage=MemoryStorage())
+    await setup_handlers(dp)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
