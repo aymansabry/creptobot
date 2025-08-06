85eeb5014e7725ce34af78609d6c3c19acefbcbb
aymansabry <zeioszeios0@gmail.com>
2025-08-06 13:15:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from handlers.buttons import deals, wallet
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# تسجيل handlers
+# تسجيل ال handlers
 dp.register_message_handler(wallet.create_wallet, text="💰 إنشاء محفظة")
 dp.register_callback_query_handler(deals.handle_real_deal, text_startswith="deal_")
 
 async def main():
-    if config.BINANCE_API_URL == "https://testnet.binance.vision":
-        print("⚠️ يعمل البوت في وضع الاختبار")
+    print("✅ البوت يعمل بنجاح!")
     await dp.start_polling()
 
 if __name__ == "__main__":
