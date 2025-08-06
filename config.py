287a475b903e29f30d59c77464691a138b023116
aymansabry <zeioszeios0@gmail.com>
2025-08-06 12:23:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
-import os
-from dotenv import load_dotenv
+from decouple import config
 
-load_dotenv()
+class Config:
+    # Binance
+    BINANCE_API_KEY = config("REAL_BINANCE_API_KEY")
+    BINANCE_SECRET = config("REAL_BINANCE_SECRET")
+    
+    # Tron
+    TRON_PRIVATE_KEY = config("ENCRYPTED_TRON_KEY")  # مشفرة
+    ADMIN_WALLET = config("ADMIN_TRON_WALLET")
+    
+    # Security
+    ENCRYPTION_KEY = config("ENCRYPTION_MASTER_KEY")
+    ALLOWED_USERS = [int(u) for u in config("ALLOWED_USER_IDS").split(",")]
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
-BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
-TRON_WALLET_ADDRESS = os.getenv("TRON_WALLET_ADDRESS")
-TRON_PRIVATE_KEY = os.getenv("TRON_PRIVATE_KEY")
-OWNER_TELEGRAM_ID = int(os.getenv("OWNER_TELEGRAM_ID"))
-DATABASE_URL = os.getenv("DATABASE_URL")
-AI_ENGINE_API_KEY = os.getenv("AI_ENGINE_API_KEY")
-
+config = Config()
