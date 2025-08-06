dec7176371ee2a1fa4d03021029da0a9c4d6dd9d
aymansabry <zeioszeios0@gmail.com>
2025-08-06 16:50:55 +0300

    Delete database directory

--- database/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, BigInteger
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(BigInteger, primary_key=True)
-    wallet_address = Column(String(64))
