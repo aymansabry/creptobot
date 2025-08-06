eaca75f06768dd791bece8afdb34401c01cb828d
aymansabry <zeioszeios0@gmail.com>
2025-08-06 13:30:11 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,20 +1,10 @@
 from sqlalchemy import Column, String, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
-from database.encryption import CryptoManager
 from config import config
 
 Base = declarative_base()
-crypto = CryptoManager(config.ENCRYPTION_KEY)
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(BigInteger, primary_key=True)
-    encrypted_wallet = Column(String(64))
-    
-    @property
-    def wallet(self):
-        return crypto.decrypt(self.encrypted_wallet)
-    
-    @wallet.setter
-    def wallet(self, value):
-        self.encrypted_wallet = crypto.encrypt(value)
+    wallet_address = Column(String(64))
