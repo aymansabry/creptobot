8ffb12aaa0ce40fdc69d0d751ef69c5d15077fc9
aymansabry <zeioszeios0@gmail.com>
2025-08-06 12:42:57 +0300

    Create security.py

---
+++ core/security.py
@@ -0,0 +1,12 @@
+from cryptography.fernet import Fernet
+import base64
+
+class SecureVault:
+    def __init__(self, key: str):
+        self.cipher = Fernet(base64.urlsafe_b64encode(key.encode().ljust(32)[:32]))
+    
+    def encrypt(self, data: str) -> str:
+        return self.cipher.encrypt(data.encode()).decode()
+    
+    def decrypt(self, encrypted: str) -> str:
+        return self.cipher.decrypt(encrypted.encode()).decode()
