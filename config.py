82f6ab378794590b550ca44788d0346f46b76a85
aymansabry <zeioszeios0@gmail.com>
2025-08-06 13:28:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,12 @@
 from decouple import config
 
 class Config:
-    # Telegram
     BOT_TOKEN = config("BOT_TOKEN")
-    
-    # Binance
     BINANCE_API_KEY = config("BINANCE_API_KEY")
     BINANCE_SECRET = config("BINANCE_SECRET")
-    BINANCE_API_URL = config("BINANCE_API_URL", "https://api.binance.com")
-    
-    # Tron
-    TRONGRID_API_URL = "https://api.trongrid.io"
-    TRONGRID_API_KEY = config("TRONGRID_API_KEY")  # احصل عليه من trangrid.io
-    TRON_USDT_CONTRACT = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
-    
-    # Security
+    TRONGRID_API_KEY = config("TRONGRID_API_KEY")
+    ADMIN_WALLET = config("ADMIN_WALLET")
     ENCRYPTION_KEY = config("ENCRYPTION_KEY")
-    
-    # Database
     DB_URL = config("DATABASE_URL")
 
-   
 config = Config()
