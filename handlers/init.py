1e6a2fad76561cd41ea682557559582fade0a54f
aymansabry <zeioszeios0@gmail.com>
2025-08-06 16:51:04 +0300

    Delete handlers directory

--- handlers/init.py
+++
@@ -1,5 +0,0 @@
-rom .admin import router as admin_router
-from .commands import router as commands_router
-from .deals import router as deals_router
-
-__all__ = ['admin_router', 'commands_router', 'deals_router']
