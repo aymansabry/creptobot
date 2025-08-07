56491bff17e24ea1f8ada88217fceea8ed3236b6
aymansabry <zeioszeios0@gmail.com>
2025-08-07 15:18:20 +0300

    Delete utils directory

--- utils/helpers.py
+++
@@ -1,5 +0,0 @@
-def generate_deposit_address():
-    """إنشاء عنوان إيداع فريد لكل مستخدم"""
-    from hashlib import sha256
-    from datetime import datetime
-    return sha256(f"{datetime.now().timestamp()}".encode()).hexdigest()[:20]
