693131b82406ed0677fe0ac70c93269f15f4b7f0
aymansabry <zeioszeios0@gmail.com>
2025-08-07 22:25:20 +0300

    Update config_loader.py

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -8,4 +8,10 @@ class ConfigLoader:
         pass
 
     def get(self, key: str, default=None):
-        return os.getenv(key, default)
+        parts = key.split(".")
+        if len(parts) == 2:
+            prefix, field = parts
+            env_key = f"{prefix.upper()}_{field.upper()}"
+        else:
+            env_key = key.upper()
+        return os.getenv(env_key, default)
