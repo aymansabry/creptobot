c6c48d8620cc5754765e79e49e5df001fb104384
aymansabry <zeioszeios0@gmail.com>
2025-08-07 11:53:37 +0300

    Delete core directory

--- core/keyboards.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def main_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="📊 عرض الصفقات", callback_data="view_deals")],
-        [InlineKeyboardButton(text="💸 الاستثمار الآن", callback_data="start_invest")],
-        [InlineKeyboardButton(text="🧾 حالة الاستثمار", callback_data="my_investments")],
-        [InlineKeyboardButton(text="📞 الدعم الفني", callback_data="contact_support")]
-    ])
--- core/utils.p
+++
@@ -1,5 +0,0 @@
-import random
-import string
-
-def generate_tx_id():
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
