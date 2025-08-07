6169bb3b1e16c87d81bc9dc23101fbd44614c29b
aymansabry <zeioszeios0@gmail.com>
2025-08-07 15:07:54 +0300

    Delete db directory

--- db/crud.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from db.models import Trade
-
-def record_trade(db: Session, trade_data: dict):
-    trade = Trade(**trade_data)
-    db.add(trade)
-    db.commit()
-    db.refresh(trade)
-    return trade
