401df05606bb16f60d0d7b9c9828d4175851d866
aymansabry <zeioszeios0@gmail.com>
2025-08-07 10:31:04 +0300

    Delete tests directory

--- tests/test_binance.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from binance.central_wallet import CentralWallet
-
-class TestBinanceIntegration(unittest.TestCase):
-    def test_balance_fetch(self):
-        wallet = CentralWallet()
-        balance = wallet.get_balance('USDT')
-        self.assertIsInstance(balance, float)
