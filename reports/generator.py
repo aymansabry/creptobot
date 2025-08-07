f8c43e2c33b62df39b074188710ca8be41f3cd23
aymansabry <zeioszeios0@gmail.com>
2025-08-07 10:30:23 +0300

    Delete reports directory

--- reports/generator.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from db.models import Transaction
-
-def generate_user_report(user_id: str):
-    transactions = Transaction.query.filter_by(user_id=user_id).all()
-    df = pd.DataFrame([t.__dict__ for t in transactions])
-    return df.to_markdown(tablefmt="grid")
