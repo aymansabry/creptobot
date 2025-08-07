b379471ff4d635a9d0d8b8964d85b60d4c77faa9
aymansabry <zeioszeios0@gmail.com>
2025-08-07 14:17:46 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,23 @@
+from .deposit import (
+    start_deposit,
+    receive_deposit_amount,
+    verify_transaction,
+    cancel_deposit,
+    DEPOSIT_AMOUNT,
+    DEPOSIT_CONFIRM
+)
+from .trading import start_trading, execute_trade
+from .wallet import show_balance, start
 
+__all__ = [
+    'start_deposit',
+    'receive_deposit_amount',
+    'verify_transaction',
+    'cancel_deposit',
+    'DEPOSIT_AMOUNT',
+    'DEPOSIT_CONFIRM',
+    'start_trading',
+    'execute_trade',
+    'show_balance',
+    'start'
+]
