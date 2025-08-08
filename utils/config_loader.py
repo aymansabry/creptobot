e781b5f95695863b55990d3633ea8e0c0e9fd20f
aymansabry <zeioszeios0@gmail.com>
2025-08-08 12:30:11 +0300

    Delete utils directory

--- utils/config_loader.py
+++
@@ -1,7 +0,0 @@
-# utils/config_loader.py
-import os
-
-class ConfigLoader:
-    def get(self, key: str, default=None):
-        env_key = key.upper().replace('.', '_')
-        return os.environ.get(env_key, default)
