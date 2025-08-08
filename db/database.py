a8437341a29940600a2ff53ffdedab1bc03a2bd2
aymansabry <zeioszeios0@gmail.com>
2025-08-08 22:53:33 +0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -5,16 +5,4 @@ from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=False)
-
-async_session = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
-
-async def get_session() -> AsyncSession:
-    """
-    Dependency to get a database session.
-    """
-    async with async_session() as session:
-        yield session
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
