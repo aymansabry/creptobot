f03f8db55e9bba17519b7a3eebf27068b4d355ba
aymansabry <zeioszeios0@gmail.com>
2025-08-08 14:59:01 +0300

    Delete src directory

--- src/handlers.py
+++
@@ -1,4 +0,0 @@
-# src/handlers.py
-
-def setup_handlers(app):
-    print("✅ تم إعداد الـ Handlers")
--- src/handlers/user.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes, CommandHandler, Application
-from ui.menus import user_main_menu
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("أهلاً بك في البوت!", reply_markup=user_main_menu())
-
-def user_handlers(app: Application):
-    app.add_handler(CommandHandler("start", start))
