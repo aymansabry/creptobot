586ac97cd6f102f160767a5133256be5b103ed1f
aymansabry <zeioszeios0@gmail.com>
2025-08-08 15:45:26 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,12 @@
 from .start import start_handler
+from .admin import admin_handler
 from .balance import balance_handler
 from .portfolio import portfolio_handler
 from .trade import trade_handler
-from .admin import admin_handler
 
 def setup_handlers(app):
     app.add_handler(start_handler)
+    app.add_handler(admin_handler)
     app.add_handler(balance_handler)
     app.add_handler(portfolio_handler)
     app.add_handler(trade_handler)
-    app.add_handler(admin_handler)
