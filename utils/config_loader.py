cc8b25c98b59fe9831c7f6ba9d9b7d62d28fb3af
aymansabry <zeioszeios0@gmail.com>
2025-08-08 09:31:32 +0300

    Update config_loader.py

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,17 +1,7 @@
+# utils/config_loader.py
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class ConfigLoader:
-    def __init__(self):
-        pass
-
     def get(self, key: str, default=None):
-        parts = key.split(".")
-        if len(parts) == 2:
-            prefix, field = parts
-            env_key = f"{prefix.upper()}_{field.upper()}"
-        else:
-            env_key = key.upper()
-        return os.getenv(env_key, default)
+        env_key = key.upper().replace('.', '_')
+        return os.environ.get(env_key, default)
