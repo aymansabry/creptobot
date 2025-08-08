b928440968c89bdbe55dc72c1422fee80a81d954
aymansabry <zeioszeios0@gmail.com>
2025-08-08 18:23:32 +0300

    Delete handlers directory

--- handlers/portfolio.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
-
-async def portfolio(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📊 محفظتك حالياً فارغة.")
-
-portfolio_handler = CommandHandler("portfolio", portfolio)
