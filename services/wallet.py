d8bbd6b513aefa278d4eb5d0fc529b605c0588ea
aymansabry <zeioszeios0@gmail.com>
2025-08-08 18:23:41 +0300

    Delete services directory

--- services/wallet.py
+++
@@ -1,9 +0,0 @@
-from database import AsyncSessionLocal
-from database.models import User
-from sqlalchemy import select
-
-async def get_balance(user_id: int):
-    async with AsyncSessionLocal() as session:
-        result = await session.execute(select(User).where(User.id == user_id))
-        user = result.scalar_one_or_none()
-        return user.balance if user else 0.0
