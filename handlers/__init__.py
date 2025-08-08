39e1fd0ef936df177074836785f4e79eaf12887d
aymansabry <zeioszeios0@gmail.com>
2025-08-08 15:21:07 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,14 +1,12 @@
-from telegram.ext import Application
 from .start import start_handler
 from .balance import balance_handler
 from .portfolio import portfolio_handler
 from .trade import trade_handler
 from .admin import admin_handler
 
-def setup_handlers(app: Application):
+def setup_handlers(app):
     app.add_handler(start_handler)
     app.add_handler(balance_handler)
     app.add_handler(portfolio_handler)
     app.add_handler(trade_handler)
     app.add_handler(admin_handler)
-    print("✅ تم إعداد الـ Handlers")
