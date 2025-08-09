5367ceea619a221f35dbb856f5fe1d9711da4a3c
aymansabry <zeioszeios0@gmail.com>
2025-08-09 21:02:01 +0300

    Update and rename init.py to __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -4,6 +4,3 @@ from .session import engine
 def init_db():
     Base.metadata.create_all(bind=engine)
     print("Database tables created successfully.")
-
-if __name__ == "__main__":
-    init_db()
