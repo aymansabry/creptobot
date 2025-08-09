0265e227ce02b8c2682341bfd0c7c0ebf7b6f895
aymansabry <zeioszeios0@gmail.com>
2025-08-09 16:50:14 +0300

    Update admin_handler.py

--- handlers/admin_handler.py
+++ handlers/admin_handler.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from db.models import User
