36eaea324fcc5800f0d4c1bea429d23b7a0add69
aymansabry <zeioszeios0@gmail.com>
2025-08-09 11:28:10 +0300

    Delete db directory

--- db/database.py
+++
@@ -1,8 +0,0 @@
-# project_root/db/database.py
-
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-from core.config import settings
-
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
