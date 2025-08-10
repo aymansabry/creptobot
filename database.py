edce3e50796f57e224341bfaa5155d4ffd5abecb
aymansabry <zeioszeios0@gmail.com>
2025-08-10 17:32:04 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,19 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL not set in .env")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 def create_tables():
+    print("جاري إنشاء الجداول في قاعدة البيانات...")
     Base.metadata.create_all(bind=engine)
+    print("تم إنشاء الجداول بنجاح.")
