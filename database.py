2b4b3eb6cb1ff2efca74bf6781a808470111b32e
aymansabry <zeioszeios0@gmail.com>
2025-08-10 19:30:27 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL environment variable is missing")
+from sqlalchemy.orm import sessionmaker
+from models import Base
+from settings import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
