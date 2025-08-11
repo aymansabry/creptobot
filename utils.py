1fd2b6b4dc0271ad057a0d6e1d158e38145c71fc
aymansabry <zeioszeios0@gmail.com>
2025-08-11 10:28:23 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-from settings import fernet
-
-def encrypt_api_keys(data: str) -> str:
-    return fernet.encrypt(data.encode()).decode()
-
-def decrypt_api_keys(token: str) -> str:
-    return fernet.decrypt(token.encode()).decode()
