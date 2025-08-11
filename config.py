9a313d22d9f8b7c87b86e960cbb6fec3bb8c43b0
aymansabry <zeioszeios0@gmail.com>
2025-08-11 23:27:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,12 @@ class Config:
     
     # تعديل هنا لضبط تنسيق رابط قاعدة البيانات
     raw_db_url = os.getenv("DATABASE_URL")
-    if raw_db_url.startswith("mysql://"):
+    if raw_db_url and raw_db_url.startswith("mysql://"):
         DATABASE_URL = raw_db_url.replace("mysql://", "mysql+pymysql://")
     else:
         DATABASE_URL = raw_db_url
     
     ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY", Fernet.generate_key().decode())
     ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "").split(",") if id]
+    ADMIN_ID = ADMIN_IDS[0] if ADMIN_IDS else None  # إضافة هذا السطر
     MIN_INVESTMENT = float(os.getenv("MIN_INVESTMENT", 10.0))
