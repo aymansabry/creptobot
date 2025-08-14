8048703af7c87829afdc202ac3250715be6951f3
aymansabry <zeioszeios0@gmail.com>
2025-08-14 22:32:03 +0300

    Update user_menu.py

--- menus/user_menu.py
+++ menus/user_menu.py
@@ -1,22 +1,14 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def user_main_menu_keyboard() -> InlineKeyboardMarkup:
+def user_main_menu_keyboard():
     kb = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="إدارة بيانات التداول", callback_data="user_manage_trading")],
-            [InlineKeyboardButton(text="ابدأ الاستثمار", callback_data="user_start_investment")],
-            [InlineKeyboardButton(text="استثمار وهمي", callback_data="user_demo_investment")],
-            [InlineKeyboardButton(text="كشف حساب", callback_data="user_account_statement")],
-            [InlineKeyboardButton(text="إيقاف الاستثمار", callback_data="user_stop_investment")],
-            [InlineKeyboardButton(text="حالة السوق", callback_data="market_status")]
-        ]
-    )
-    return kb
-
-def amount_input_keyboard() -> InlineKeyboardMarkup:
-    kb = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="الرجوع", callback_data="back_to_trading_setup")]
+            [InlineKeyboardButton("إدارة بيانات التداول", callback_data="user_manage_trading")],
+            [InlineKeyboardButton("ابدأ الاستثمار", callback_data="user_start_investment")],
+            [InlineKeyboardButton("استثمار وهمي", callback_data="user_demo_investment")],
+            [InlineKeyboardButton("كشف حساب", callback_data="user_account_statement")],
+            [InlineKeyboardButton("إيقاف الاستثمار", callback_data="user_stop_investment")],
+            [InlineKeyboardButton("حالة السوق", callback_data="market_status")]
         ]
     )
     return kb
