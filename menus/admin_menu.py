965c018a3d1e52ac32aff34c2fceda61eaf9c468
aymansabry <zeioszeios0@gmail.com>
2025-08-14 22:40:23 +0300

    Update admin_menu.py

--- menus/admin_menu.py
+++ menus/admin_menu.py
@@ -4,10 +4,10 @@ def admin_main_menu_keyboard():
     kb = InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton("تعديل نسبة ربح البوت", callback_data="admin_edit_bot_profit")],
-            [InlineKeyboardButton("عدد المستخدمين الإجمالي", callback_data="admin_total_users")],
-            [InlineKeyboardButton("عدد المستخدمين أونلاين", callback_data="admin_online_users")],
-            [InlineKeyboardButton("تقارير الاستثمار", callback_data="admin_investment_reports")],
-            [InlineKeyboardButton("حالة البوت", callback_data="admin_bot_status")],
+            [InlineKeyboardButton("عدد المستخدمين اجمالي", callback_data="admin_total_users")],
+            [InlineKeyboardButton("عدد المستخدمين اونلاين", callback_data="admin_online_users")],
+            [InlineKeyboardButton("تقارير الاستثمار اجمالا عن فترة", callback_data="admin_investment_report")],
+            [InlineKeyboardButton("حالة البوت برمجيا", callback_data="admin_bot_status")],
             [InlineKeyboardButton("التداول كمستخدم عادي", callback_data="admin_trade_as_user")]
         ]
     )
