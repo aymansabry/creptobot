5eea7e78112f89e85bd03d4c57d4e303af9741c4
aymansabry <zeioszeios0@gmail.com>
2025-08-14 22:40:53 +0300

    Update user_menu.py

--- menus/user_menu.py
+++ menus/user_menu.py
@@ -3,12 +3,12 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def user_main_menu_keyboard():
     kb = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton("إدارة بيانات التداول", callback_data="user_manage_trading")],
-            [InlineKeyboardButton("ابدأ الاستثمار", callback_data="user_start_investment")],
+            [InlineKeyboardButton("تسجيل أو تعديل بيانات التداول", callback_data="user_edit_exchange")],
+            [InlineKeyboardButton("ابدأ استثمار", callback_data="user_start_investment")],
             [InlineKeyboardButton("استثمار وهمي", callback_data="user_demo_investment")],
-            [InlineKeyboardButton("كشف حساب", callback_data="user_account_statement")],
-            [InlineKeyboardButton("إيقاف الاستثمار", callback_data="user_stop_investment")],
-            [InlineKeyboardButton("حالة السوق", callback_data="market_status")]
+            [InlineKeyboardButton("كشف حساب عن فترة", callback_data="user_account_statement")],
+            [InlineKeyboardButton("حالة السوق", callback_data="user_market_status")],
+            [InlineKeyboardButton("إيقاف الاستثمار", callback_data="user_stop_investment")]
         ]
     )
     return kb
