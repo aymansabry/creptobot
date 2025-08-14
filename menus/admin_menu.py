ef64f5ec9f888ed4a5377c3b67432feafbb4b9b0
aymansabry <zeioszeios0@gmail.com>
2025-08-14 23:20:33 +0300

    Update admin_menu.py

--- menus/admin_menu.py
+++ menus/admin_menu.py
@@ -1,14 +1,11 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def admin_main_menu_keyboard():
-    kb = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton("تعديل نسبة ربح البوت", callback_data="admin_edit_bot_profit")],
-            [InlineKeyboardButton("عدد المستخدمين اجمالي", callback_data="admin_total_users")],
-            [InlineKeyboardButton("عدد المستخدمين اونلاين", callback_data="admin_online_users")],
-            [InlineKeyboardButton("تقارير الاستثمار اجمالا عن فترة", callback_data="admin_investment_report")],
-            [InlineKeyboardButton("حالة البوت برمجيا", callback_data="admin_bot_status")],
-            [InlineKeyboardButton("التداول كمستخدم عادي", callback_data="admin_trade_as_user")]
-        ]
-    )
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton("تعديل نسبة ربح البوت", callback_data="admin_edit_bot_profit"))
+    kb.add(InlineKeyboardButton("عدد المستخدمين الإجمالي", callback_data="admin_total_users"))
+    kb.add(InlineKeyboardButton("عدد المستخدمين أونلاين", callback_data="admin_online_users"))
+    kb.add(InlineKeyboardButton("تقارير الاستثمار", callback_data="admin_investment_reports"))
+    kb.add(InlineKeyboardButton("حالة البوت", callback_data="admin_bot_status"))
+    kb.add(InlineKeyboardButton("التداول كمستخدم عادي", callback_data="admin_trade_as_user"))
     return kb
