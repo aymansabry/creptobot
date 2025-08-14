a4142f945116c241ab957d23e408fe2f286efdfb
aymansabry <zeioszeios0@gmail.com>
2025-08-14 22:29:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", 123456))
-DATABASE_URL = os.environ.get("DATABASE_URL")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+DATABASE_URL = os.environ.get("DATABASE_URL")  # MySQL
+ADMIN_ID = int(os.environ.get("ADMIN_ID", 123456))
+ENCRYPTION_KEY = os.environ.get("ENCRYPTION_KEY")  # مفتاح Fernet للتشفير
