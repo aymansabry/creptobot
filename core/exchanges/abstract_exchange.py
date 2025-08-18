dd296c81e83da2d4cfe6e4d80eaf3b5141610ee2
aymansabry <zeioszeios0@gmail.com>
2025-08-18 15:20:25 +0300

    Update abstract_exchange.py

--- core/exchanges/abstract_exchange.py
+++ core/exchanges/abstract_exchange.py
@@ -3,20 +3,25 @@ from abc import ABC, abstractmethod
 
 class AbstractExchange(ABC):
     def __init__(self, api_key: str, api_secret: str):
-        self.exchange = None
         self.api_key = api_key
         self.api_secret = api_secret
-    
+        self.exchange = None
+        self.fee = 0.0
+
     @abstractmethod
     def connect(self):
         pass
-    
+
     @abstractmethod
     def validate_credentials(self):
         pass
-    
+
     def fetch_balance(self):
+        if not self.exchange:
+            self.connect()
         return self.exchange.fetch_balance()
-    
+
     def fetch_order_book(self, symbol: str):
+        if not self.exchange:
+            self.connect()
         return self.exchange.fetch_order_book(symbol)
