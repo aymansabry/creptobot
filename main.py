e734fa447a5af09abc766d7301f444483302512a
aymansabry <zeioszeios0@gmail.com>
2025-08-18 15:22:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from bots.telegram_bot import main as start_bot
 from db.database import init_db
 
-if __name__ == '__main__':
+def main():
+    # تهيئة قاعدة البيانات
     init_db()
+    
+    # بدء بوت التليجرام
     start_bot()
+
+if __name__ == '__main__':
+    main()
