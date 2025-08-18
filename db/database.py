b713c2c2ef75d7f65a40b1c10782c286e0541f48
aymansabry <zeioszeios0@gmail.com>
2025-08-18 15:22:58 +0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,15 +1,20 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .models import Base
 from config import Config
 
 engine = create_engine(Config.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
+
 def init_db():
+    """تهيئة قاعدة البيانات وإنشاء الجداول"""
+    import db.models  # لا تحذف هذا السطر رغم أنه غير مستخدم مباشرة
     Base.metadata.create_all(bind=engine)
 
 def get_db():
+    """الحصول على جلسة قاعدة البيانات"""
     db = SessionLocal()
     try:
         yield db
