669d6b83d88a8a92079fae6c203f91264037e8f1
aymansabry <zeioszeios0@gmail.com>
2025-08-19 22:16:08 +0300

    Delete core directory

--- core/exchanges/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .binance import BinanceExchange
-from .coinbase import CoinbaseExchange
-from .abstract_exchange import AbstractExchange
-
-__all__ = ['BinanceExchange', 'CoinbaseExchange', 'AbstractExchange']
