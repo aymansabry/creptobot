2d422dd1495c7af7618617187fd29e402218e57e
aymansabry <zeioszeios0@gmail.com>
2025-08-23 15:08:30 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
