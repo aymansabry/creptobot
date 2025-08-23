1a9eb0098e390d3fb55a69e6c835aa7ca4db5304
aymansabry <zeioszeios0@gmail.com>
2025-08-23 15:07:10 +0300

    Delete bot directory

--- bot/settingd.py
+++
@@ -1,9 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def settings_menu():
-    keyboard = [
-        [InlineKeyboardButton("🔑 ربط Binance", callback_data='link_binance')],
-        [InlineKeyboardButton("💰 تعديل الرصيد", callback_data='edit_balance')],
-        [InlineKeyboardButton("🔙 رجوع", callback_data='back_to_main')]
-    ]
-    return InlineKeyboardMarkup(keyboard)
