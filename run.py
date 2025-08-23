b8f7686385465ca3ffa1db7223604753bd93a391
aymansabry <zeioszeios0@gmail.com>
2025-08-23 18:01:05 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import asyncio
-from bot import main as bot_main  # استدعاء دالة main من bot.py
+from bot import main
 
 if __name__ == "__main__":
-    # تشغيل البوت
-    asyncio.run(bot_main())
+    asyncio.run(main())
